395dd9f649543be9ceae5c6f0cab3244d468226d
abe <105024329+abe444@users.noreply.github.com>
2023-07-04 20:20:58 -0500

    Update CONFIGURATION.php
    Changes to functionality....for now.

--- CONFIGURATION.php
+++ CONFIGURATION.php
@@ -3,8 +3,8 @@
 $CONFIGURATION = [
     // SITE INDEX
     'SITE_NAME'                                     => 'DA ECHOBUBBS',
-    'SITE_DESCRIPTION'                              => 'The <strong>ECHOBUBBLE</strong> is a shameless nod to the now defunct <strong><a href="https://github.com/faissaloo/ratmachine" alt="OG ratmachine">ratwires.space</a></strong> textboard.',
-    'POST_LIMIT'                                    => 3,
+    'SITE_DESCRIPTION'                              => 'The <strong>ECHOBUBBLE</strong> is a shameless nod to the now defunct <strong><a href="https://github.com/faissaloo/ratmachine" alt="OG ratmachine">ratwires.space</a></strong> textboard. There is a posting limit of 255, after which the JSON database file gets obliterated.',
+    'POST_LIMIT'                                    => 255,
     
     // POSTING
     'MIN_MESSAGE_LENGTH'                            => 5,
