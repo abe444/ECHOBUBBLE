b4ddbd504bea84a18b4ec962bbfba9789ce6f2ab
abe <105024329+abe444@users.noreply.github.com>
2023-07-05 22:58:37 -0500

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,5 @@
 <hr>
 <footer style="text-align:center;text-shadow: 1px 1px 2px black;"><strong><a href="<?php echo $CONFIGURATION['FREN_1']?>" alt="Bosnian 2channel">čudan</a> ~ <a href="<?php echo $CONFIGURATION['FREN_2']?>" alt="REALTIME HYPERCONNECTIVITY">somachat</a> ~ <a href="<?php echo $CONFIGURATION['FREN_3']?>" alt="RIP. A textboard hybrid that was ahead of its time.">textbunker [RIP]</a></strong></footer>
+    </main>
+  </body>
+</html>
