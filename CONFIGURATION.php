4afb66ef48d4b4d712fdec5a8a1cb9d42ad79f4e
abe <105024329+abe444@users.noreply.github.com>
2023-10-07 14:04:58 -0500

    Add files via upload

---
+++ CONFIGURATION.php
@@ -0,0 +1,18 @@
+<?php 
+
+$CONFIGURATION = [
+    // SITE INFO
+    'SITE_NAME'                                     => 'ECHOBUBBLE',
+    'SITE_DESCRIPTION'                              => 'The ECHOBUBBLE is a shameless nod to the now defunct ratwires.space textboard.',
+
+    // MESSAGE
+    'MAX_LINE_BREAKS'				                => 45,
+    'MAX_WORD_LENGTH'                               => 50,
+    'MIN_MESSAGE_LENGTH'                            => 3,
+    'MAX_MESSAGE_LENGTH'                            => 2500,
+
+    // TITLE
+    'MIN_TITLE_LENGTH'                              => 5,
+    'MAX_TITLE_LENGTH'                              => 100,
+
+];
